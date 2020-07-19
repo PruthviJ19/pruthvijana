33c5d7415a58c39d72c5daf2e4afaaae66654915
PruthviJ19 <68417132+PruthviJ19@users.noreply.github.com>
2020-07-19 21:28:04 +0530

    Add files via upload

---
+++ pj3.py
@@ -0,0 +1,28 @@
+def pj(colour = "black"):#decalare the parameter
+    print("i like " + colour)#add stmt to printed
+
+pj("blue")#declare the colours to be exicuted
+pj("pink")
+pj()
+pj("blue")            # using default parameter 
+
+
+
+def jana(languages):#declare the parameter with lang
+    for x in lang:#check condition of x
+        #print(x)#if true x gets printed
+        
+    languages = ["telgu","english","hindi"]#initialise the lang
+    
+jana(languages)#lang gets exicuted 
+
+
+
+def pj(colour = "black"):
+    print("i like " + colour)
+
+pj("blue")
+pj("pink")
+pj()
+pj("blue")
+    
