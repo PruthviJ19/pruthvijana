f29da14a0bc19256a1eca9c5d16f1d10a9e99b86
PruthviJ19 <68417132+PruthviJ19@users.noreply.github.com>
2020-07-19 20:05:18 +0530

    Add files via upload

---
+++ pj2.py
@@ -0,0 +1,49 @@
+def j(jp):
+   # print(jp +  " jana ")# to print the name with lastname
+    
+j("prajwal")              #give 3 names to be printed
+j("ajit")
+j("amit")                                             #aguments function
+
+
+def j(pruthvi,jana):
+    #print(pruthvi + " " + jana)
+    
+ #j(" jana ","PJ")                                    #passing 1 parameter  
+
+
+def j(*friends):
+ # print("number of friends is " + friends[2])
+    
+j("jana","hi","3")   
+                                                     # using arbitary * operator#
+#giving the value to the tuple
+
+
+
+def pj(**name):
+   # print("her pet name is " + name["pname"])
+    
+pj(name = "sj", pname = "suspense")                  #using ** unknown operator
+
+
+
+def pj(goa,blg,manglur):
+   # print("thrilling place is " + goa ) #for the display stmt
+    
+pj(goa  = "wow",blg = "busy life", manglur ="cool")  #using key values
+#extracting information from calling function
+
+def my_function(x):
+    return 5 * x
+
+#print(my_function) 
+    
+
+
+
+
+
+ 
+ 
+ 
