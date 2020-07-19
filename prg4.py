405a959c2635c20b68c800c433031cc1725e9bfb
PruthviJ19 <68417132+PruthviJ19@users.noreply.github.com>
2020-07-19 21:19:45 +0530

    Add files via upload

---
+++ prg4.py
@@ -0,0 +1,34 @@
+
+
+ # SET: it is collection of items that cannot be modified,ordered,mutable.
+
+days=set(["mon","tue","wed","thu","fri","sat","sun"])#initailise the set
+months={"jan","feb"}#initialise the set
+#print(days)#to print days
+#print(months)#to print months              # example for set
+
+
+
+Days=set(["mon","tue","wed","thu"])#initialise the set of days
+ 
+for d in Days:#for loop to display the set
+  #print(d) #print days                     #using for loop
+    
+ a = 40#assign value to a
+ b = 60#assign value to b
+ if b >a:# check the condtion 
+    #print("b is greater than a")#to print if the above condition is true     #if statement
+
+     
+ a =44#assign value to a
+ b =66#assign value to b
+ if b > a :#check condition
+   print("b is greater")#to exicute if true condition
+ elif a == b:#equality condition
+   print("both are equal")#to exicute if equality cond is true
+ else:
+   print("a is greater")            #nested if
+
+       
+    
+    
