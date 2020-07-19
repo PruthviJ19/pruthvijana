2cea286ff62ac69df3161779a21b7146428bd604
PruthviJ19 <68417132+PruthviJ19@users.noreply.github.com>
2020-07-19 21:12:23 +0530

    Add files via upload

---
+++ prg3.py
@@ -0,0 +1,99 @@
+x= "python is " #declaration of x
+y =" awesome"   #declaraton of y
+z= x+y          #concatination ie combining x and y stmts
+#print(z)       #print the result z
+
+
+y="hi"          #declare y
+#print(y)       #print y
+
+a="pruthvi"         #declare stmt to a                 
+#print(len(a))      #checking the length of a                     #length of stmt
+
+p="hello, world!"         #assigning the stmt to p
+#print(p.replace("w","d")) #replacement of letter
+
+q="hello"           #declare word to q and r
+r="world"           
+s=q+"  " +r         #adding of 2 stmts
+#print(s)           #print the result s               #concatination
+
+age=30#assign age 
+txt="my brother is bodybuilder,he is {}"#add text to be added before age
+#print(txt.format(age))#print output  
+
+x=15#assign value
+y=2#assign value
+
+  #print(x//y)#modulus operation gets printed
+
+thislist=["apple","cherry","mango"]#initialise list of varibles
+#print(thislist)#list is printed
+
+
+list=["apple","apple"]#initialise value of list with same word
+#print(list)#list duplication execution  
+
+  
+list=["apple","banana"]#assigning the varibles to list
+#print(list[1])#displaying the variable by assigning the index value
+
+
+
+list=["apple","banana","cherry"]#creat a list of words 
+#print(list[-1])#negative list execution
+
+
+
+list=["apple","banana","chery"]#initialise the list
+#print(list[:1])#specific index valued varible gets printed
+
+                   #index 
+
+list=["apple","banana","cherry","orange","kiwi","melon","mango"]
+#print(list[-4:-2])     #specific range to index
+
+           
+
+list=["apple","banana","cherry"]#intilise the list
+list[1]="blackcurrant"#specify the index value and word to be replaced
+#print(list)#new list is obtained
+             #change value
+
+
+
+list=["apple","cherry","banana"]#initilise the list
+if "apple" in list:#condition to check the element in the list
+# print("yes")#if condition is true                  
+#print(len(list))#to check lentgh of list
+    
+        
+    
+ thistuple = ("apple","banana","cherry")#initialise the tuple
+ print(thistuple)#print the tuple
+                         #tuple
+ 
+ 
+ tuple=("apple","banana","cherry")
+ #print(tuple[-1])#to obtain the last word,,negative index                  #print last item of tuple
+ 
+ 
+ 
+ tuple=("apple","banana","cherry","orange","kiwi","melon","mango")
+ #print(tuple[2:5])  #to print specific varibles providiv index range                       #return 3rd,4th,5th item
+  
+ 
+ 
+ list=["apple","banana","cherry","orange","kiwi","melon","mango"]
+ #print(list[2:5])#covert tuple tp list
+ 
+x=("apple","banana","cherry")#assign some tuple 
+#y=list(x)#print x value
+y[1]="kiwi"#replaceing word with index value
+x=tuple(y)#new tuple is created
+
+#print(x)#convert tuple into list
+
+
+ 
+
