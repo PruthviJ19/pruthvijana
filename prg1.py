ff837f5bc6b13d7cdefacbf414eaece0c8cdeca9
PruthviJ19 <68417132+PruthviJ19@users.noreply.github.com>
2020-07-19 20:28:22 +0530

    Add files via upload

---
+++ prg1.py
@@ -0,0 +1,51 @@
+
+a = 2     #assigning the values
+b = 330
+#print("A") if a > b else print("B")        #using if and else in 1 line
+#to print the greater value
+
+a = 330   #assigning the values
+b = 330
+#print("A") if a > b else print("=") if a == b else print("B")
+ #to check the equality                                          # one line if else stmt
+
+a = 80    #initialising the values
+b = 60
+c = 90
+if a > b and c > a:
+    #print("both r true state")            # and operator
+#comparing the  btw 3 values
+
+    
+ x = 38           #assign the value of x
+ if x > 10:             #checking the condtion of x
+    #print("above 10,") #comparing value of x with 10
+   # if x > 20:         #checking whether x is greater than 20
+        #print("and also above 20") #to print if conditon above condition is true
+   # else:
+        #print("but not above 60")         #nested if stmts
+#checking value of x and to print else stmt       
+        
+i = 1           #initialise the i value
+while i < 5:    #checking condition of i
+   # print(i)   #to print i if condition is true
+    i += 1      3increment operation till condition gets false                          #while condition
+
+i = 1                  #assign value of i
+while i < 6:           #checking condition of i
+   # print(i)          #if true ,priniting the value i
+    if i == 3:         #if value of i equals to 3,it breaks the execution
+        break
+    i += 1               #increment oprtn if i is not equal to 3
+
+                #exit of loop
+    
+    
+for x in range(6): #assign the range to x
+   # print(x)      #print value x upto the range                        #using range()
+    
+for x in range(2,6):#assign specific range to x
+   # print(x)       #print the range                       #specific range()
+    
+
+
