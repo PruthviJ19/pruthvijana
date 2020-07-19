6cba55e1f04f07e934d7191bae8b12ba2c1473bc
PruthviJ19 <68417132+PruthviJ19@users.noreply.github.com>
2020-07-19 21:41:05 +0530

    Add files via upload

---
+++ pj6.py
@@ -0,0 +1,23 @@
+ #19th python math and code
+
+def greeting(name) :
+   # print("hi," + name)          #module creation
+
+    
+                                 
+                              
+    #in-built module
+    
+import datetime
+
+x = datetime.datetime.now()
+
+#print(x)
+
+
+x = min(2,4,5) 
+y = max(5,4,3)
+
+#print(x)
+#print(y)                        #min () and max() function
+
