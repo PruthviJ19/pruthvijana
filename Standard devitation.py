3dc3d1ed844fa033385db87a280fabd1564bf671
PruthviJ19 <68417132+PruthviJ19@users.noreply.github.com>
2020-07-31 18:30:45 +0530

    Add files via upload

---
+++ Standard devitation.py
@@ -0,0 +1,11 @@
+import numpy#library function
+speed = [32,111,138,28,59,77,97]#specify the numbers
+x = numpy.std(speed)#standard deviation formula 
+#print(x)#printing x values
+
+
+import numpy#library function
+speed = [32,111,138,28,59,77,97]#specify the numbers
+x = numpy.var(speed)#variance formula 
+#print(x)#printing x values
+
