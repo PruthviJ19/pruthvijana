9801e9de6cdc4f50d6b513253ad2625355144ebb
PruthviJ19 <68417132+PruthviJ19@users.noreply.github.com>
2020-08-04 19:22:55 +0530

    Add files via upload

---
+++ sum of 2 numb prg.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b,sum;/*initilize the varibles*/
+	printf("enter value of a;");/*to declare 1st number*/
+	scanf("%d",&a);
+	printf("enter value of b;");/*to declare 2nd number*/
+	scanf("%d",&b);
+	sum=a+b;/*formula to add the numb*/
+	printf("sum of given two numb is %d",sum);/*output stmt*/
+	getch();
+}
